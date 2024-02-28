5ff1e702f5610d968f77a95f906b20e640362109
Mohd Masoom Ansari <masoomansari09875@gmail.com>
2024-02-28 22:18:59 +0530

    DOM-1

---
+++ index.js
@@ -0,0 +1,8 @@
+let x = document.getElementById('div1');
+x.style.backgroundColor='yellow';
+x.style.fontWeight='bold';
+x.style.margin='20px';
+x.style.padding='10px';
+x.style.fontSize='18px';
+x.style.height='200px';
+x.style.width='300px';
